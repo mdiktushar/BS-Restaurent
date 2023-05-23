dcb79f666f8717ea2ee640f6d2be29b26259eeec
mdiktushar <md.tushar1804@gmail.com>
2023-05-23 22:55:59 +0600

    Featured

--- bs-restaurant-frontend/src/pages/Home/Home.jsx
+++ bs-restaurant-frontend/src/pages/Home/Home.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Banner from './components/Banner';
 import Category from './components/Category';
+import PopularMenu from './components/PopularMenu/PopularMenu';
+import Featured from './components/Featured/Featured';
 
 const Home = () => {
     return (
         <div>
             <Banner />
-
             <Category />
+            <PopularMenu />
+            <Featured />
         </div>
     );
 };
