44c4927d974de1f94cd6afa4403d9be0b6391f9a
mdiktushar <md.tushar1804@gmail.com>
2023-05-24 13:35:05 +0600

    'Parallax'

--- bs-restaurant-frontend/src/pages/Menu/Menu.jsx
+++ bs-restaurant-frontend/src/pages/Menu/Menu.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
+import Cover from "../Shared/Cover/Cover";
+import banner3 from "../../assets/menu/banner3.jpg";
+import PopularMenu from "../Home/components/PopularMenu/PopularMenu";
 
 const Menu = () => {
   return (
@@ -7,6 +10,15 @@ const Menu = () => {
       <Helmet>
         <title>BSR | Menu</title>
       </Helmet>
+
+      <Cover img={banner3} title={`Our Menu`} />
+      <PopularMenu />
+      <Cover img={banner3} title={`This is Food`} />
+      <PopularMenu />
+      <Cover img={banner3} title={`This is Salad`} />
+      <PopularMenu />
+      <Cover img={banner3} title={`This is Supe`} />
+      <PopularMenu />
     </div>
   );
 };
